efbc4cf3c46c9e338338885181c601d47879020e
Euardo7660 <eduardo.oliveira7660@gmail.com>
2023-11-11 00:18:30 -0300

    firts commit

--- App.js
+++ App.js
@@ -1,14 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
 
 export default function App() {
+  const [text, setText] = useState("ola mundo");
+ 
+  const Teste = () => {
+    setText("vai dormir"); 
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{text}</Text>
+      <Button title="click-me" onPress={Teste}/>
+      <TextInput style={styles.input} onChangeText={text => setText(text)} value={text}/>
       <StatusBar style="auto" />
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -16,5 +26,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input:{
+    height: 40,
+    width: 200,
+    borderColor: 'gray',
+    borderWidth: 1
   },
 });
