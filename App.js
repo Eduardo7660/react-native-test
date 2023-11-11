9a0bfd5ebd94b521f5a73d6ce2b37fc1a163cba6
Euardo7660 <eduardo.oliveira7660@gmail.com>
2023-11-11 00:32:14 -0300

    second commite

--- App.js
+++ App.js
@@ -1,19 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import {Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import {Button, StyleSheet, Text, TextInput, TextInputComponent, View } from 'react-native';
 import React, {useState, useEffect} from 'react';
 
 export default function App() {
   const [text, setText] = useState("ola mundo");
+  const [value, setValue] = useState(0);
+  const [value2, setValue2] = useState(0);
  
-  const Teste = () => {
-    setText("vai dormir"); 
+  const soma = () => {
+    setText(parseInt(value) + parseInt(value2)); 
   }
 
   return (
     <View style={styles.container}>
       <Text>{text}</Text>
-      <Button title="click-me" onPress={Teste}/>
-      <TextInput style={styles.input} onChangeText={text => setText(text)} value={text}/>
+      <Button title="click-me" onPress={soma}/>
+      <TextInput style={styles.input} onChangeText={value => setValue(value)} value={value}/>
+      <TextInput style={styles.input} onChangeText={value2 => setValue2(value2)} value={value2}/>
       <StatusBar style="auto" />
     </View>
   );
